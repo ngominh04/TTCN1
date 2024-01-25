a8c5821654d9248624eed7fb8de8ec2e8baaeac0
LENOVO <ngominh041103@gmail.com>
2024-01-25 21:15:50 +0700

    25/1 giỏ hàng(chưa xong)

--- src/main/java/devmaster/TTCN1/controller/CartController.java
+++ src/main/java/devmaster/TTCN1/controller/CartController.java
@@ -30,16 +30,24 @@ public class CartController {
     @GetMapping("/cart/{idCus}")
     public String showCart(Model model,@PathVariable("idCus") Integer idCus){
         model.addAttribute("cart",cartRespon.getCart(idCus));
+        List<Cart> carts = cartRespon.getCart(idCus);
+        double total = 0;
+        for (Cart cart:carts) {
+            total = total+ cart.getQuantity()*cart.getPrice();
+            model.addAttribute("total",total);
+        }
+
         return "user/cart/Cart";
     }
     @GetMapping("/add/{idCus}/{idPro}")
     public String getAddCart(Model model,@PathVariable("idCus")Integer idCus, @PathVariable("idPro") Integer idPro, HttpSession session){
         Customer customer= (Customer) session.getAttribute("saveCus");
         Product product = productRespon.findAllById(idPro.intValue());
+        boolean checkCart = false; // biến kiểm tra idPro trong giỏ
         if (product != null){
             List<Cart> carts = cartRespon.getCart(idCus);
             for (Cart cart: carts) {
-                if (product.getId() == cart.getIdPrduct()){
+                if (product.getId().equals(cart.getIdPrduct())){ // nếu tồn tại idPro của cart == id của product
                     cart.setQuantity(cart.getQuantity()+1);
                     cart.setName(product.getName());
                     cart.setPrice(product.getPrice());
@@ -47,19 +55,24 @@ public class CartController {
                     cart.setIdPrduct(product.getId());
                     cart.setIdCustomer(customer.getId());
                     cartService.save(cart);
-                }
-                else {
-                    Cart cart1 = new Cart();
-                    cart1.setName(product.getName());
-                    cart1.setPrice(product.getPrice());
-                    cart1.setQuantity(1);
-                    cart1.setImage(product.getImage());
-                    cart1.setIdPrduct(product.getId());
-                    cart1.setIdCustomer(customer.getId());
-                    cartService.save(cart1);
+                    checkCart = false;
+                    break; // thoát vòng lặp nếu tồn tại điều kiện
+                }else  {
+                    checkCart=true;
                 }
             }
+            if (checkCart){ // khi duyệt hết vòng lặp nếu checkCart == true thì tạo 1 cart mới
+                Cart cart1 = new Cart();
+                cart1.setName(product.getName());
+                cart1.setPrice(product.getPrice());
+                cart1.setQuantity(1);
+                cart1.setImage(product.getImage());
+                cart1.setIdPrduct(product.getId());
+                cart1.setIdCustomer(customer.getId());
+                cartService.save(cart1);
+            }
         }
+
 //        session.setAttribute("cartItem",cartService.findAll());
         return "redirect:/shopping_cart/cart/{idCus}";
     }
